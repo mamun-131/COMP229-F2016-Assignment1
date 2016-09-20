d194f9b6736455149c152a7c9bdc1948b12a1f73
mamun <mamun@DESKTOP-QTH7IVU>
2016-09-20 18:54:42 -0400

    done

--- COMP229-F2016-Assignment1/App_Start/RouteConfig.cs
+++ COMP229-F2016-Assignment1/App_Start/RouteConfig.cs
@@ -11,8 +11,8 @@ namespace COMP229_F2016_Assignment1
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+           // routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
